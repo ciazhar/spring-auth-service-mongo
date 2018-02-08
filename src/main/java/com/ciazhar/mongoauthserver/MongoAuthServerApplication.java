3ba1461fa2c7371c6eaeb5c82490a959197565db
ciazhar <ciazhar.id@gmail.com>
2018-02-08 14:46:56 +0700

    initial commit

---
+++ src/main/java/com/ciazhar/mongoauthserver/MongoAuthServerApplication.java
@@ -0,0 +1,12 @@
+package com.ciazhar.mongoauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoAuthServerApplication.class, args);
+	}
+}
